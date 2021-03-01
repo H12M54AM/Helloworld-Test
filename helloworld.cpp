33bf50d89a1fbf8eae3bbea28c92a1b410076386
H12M54AM <74957928+H12M54AM@users.noreply.github.com>
2021-02-28 17:40:03 -0800

    C++ Hello World
    it also has a little more

---
+++ helloworld.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main () {
+    /* std::cout is what will output your code to the terminal, "cout" is the output
+    while "cin" is the input, and that is where the user will type somehting into that input.*/
+    
+    std::cout << "Hello world";
+
+    return 0;
+}
+
+void gamer () {
+    
+}
